839d0cdbba23885e894d1d98979eb07e29af2e2a
rzpy1997 <kingradomir@gmail.com>
2023-11-07 19:39:30 +0600

    url_short_db

--- url_shorts/models.py
+++ url_shorts/models.py
@@ -2,4 +2,4 @@ from django.db import models
 
 class Link(models.Model):
     original_url = models.URLField(max_length=2000)
-    short_url = models.CharField(max_length=50, unique=True)
+    short_url = models.URLField(max_length=50, unique=True, null=True)
